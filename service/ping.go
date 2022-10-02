66d88328bd7aa9522686cf4c86d78dd17f3a8928
luhao.hale <luhao.hale@bytedance.com>
2022-10-03 01:08:55 +0800

    async post

--- service/ping.go
+++ service/ping.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -14,6 +15,20 @@ func PingHandler(w http.ResponseWriter, r *http.Request) {
 	msg, err := json.Marshal(res)
 	if err != nil {
 		fmt.Fprint(w, "内部错误")
+		return
+	}
+
+	body := &WechatMsgBody{
+		ToUserName:   "o3dzp575sUNoB3_KyJj_Aq7ZOJKw",
+		FromUserName: "gh_f61f83ed88cf",
+		CreateTime:   int(time.Now().Unix()),
+		MsgType:      "text",
+		Content:      time.Now().String(),
+	}
+	jsonData, _ := json.Marshal(body)
+	_, err = http.Post("http://api.weixin.qq.com/cgi-bin/message/custom/send", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Printf("http.Post err:%v", err)
 		return
 	}
 	w.Header().Set("content-type", "application/json")
